9c5230b55e8fffafecc879c628acc418b81bf028
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-02-02 18:32:01 -0300

    feat: Adiciona template simples de server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}.`);
+});
