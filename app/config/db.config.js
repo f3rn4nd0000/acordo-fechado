71ff2b894cf6d9ee8259e0e26e7be2c6e7aa7aa4
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-02-03 18:19:40 -0300

    feat: Adiciona .env no projeto

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv').config({path: process.cwd()+'/.env'})
+
 module.exports = {
-    url: "mongodb://localhost:27017/books_db"
+    url: "mongodb+srv://"+process.env["MONGO_LOGIN"]+":"+process.env["MONGO_PSSWD"]+"@acordo-fechado.l5oa3fl.mongodb.net/?retryWrites=true&w=majority"
 };
+// url: "mongodb://localhost:27017/books_db"
